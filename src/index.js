64343c72bb7b0f6ec7cb5974f75455a4637d9a64
Royer Pizarro <crroyerpizarro@gmail.com>
2023-03-20 09:21:56 -0500

    Agrega provider a nuestra app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import store from './redux/store';
 import "./styles/styles.scss";
-
+import "../src/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
